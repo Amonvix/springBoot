46acce44af2c95a896b534e68b455b1e41434cc8
Amonvix <hspedroso@gmail.com>
2023-07-07 14:56:04 -0300

    iniciado teste de Spring Boot - CRUD

---
+++ api/src/test/java/med/amon/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.amon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
