0ee2ab13a3433771408f27de40d42258e804f9db
Amonvix <hspedroso@gmail.com>
2023-07-24 16:32:32 -0300

    Criados todos os arquivos para a criação de medicos via POST

--- api/src/main/java/med/amon/api/controller/MedicosController.java
+++ api/src/main/java/med/amon/api/controller/MedicosController.java
@@ -1,19 +1,27 @@
 package med.amon.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.amon.api.endereco.Endereco;
 import med.amon.api.medico.DadosCadastroMedicos;
+import med.amon.api.medico.Medico;
+import med.amon.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicosController {
+	@Autowired
+	private MedicoRepository repository;
 
 	@PostMapping
+	@Transactional
 	public void cadastrar(@RequestBody DadosCadastroMedicos dados) {
-		System.out.println(dados);
+		repository.save(new Medico(dados));
 	}
 
 }
