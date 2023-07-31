89bcd6a9634687ab2261807ad97276d170631792
Amonvix <hspedroso@gmail.com>
2023-07-31 17:42:33 -0300

    Adicionada paginação de pacientes

---
+++ api/src/main/java/med/amon/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,10 @@
+package med.amon.api.paciente;
+
+public record DadosListagemPaciente( String nome, String email, String telefone, String cpf) {
+
+    public DadosListagemPaciente (Paciente paciente){
+
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf());
+    }
+    
+}
